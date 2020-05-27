96604ff35f9509c05df55b0e587c426e7c234724
lzoebaker <lzoebaker@gmail.com>
2020-05-27 10:58:04 -0600

    Preloading all images after initial page loads in order to speedup background/profile image transitions

--- portfolio/src/main/webapp/jquery_functions.js
+++ portfolio/src/main/webapp/jquery_functions.js
@@ -2,27 +2,40 @@
 * Purpose: contains various JQuery functions that listen for events on main portfolio page
 */
 
-let img_arr = new Array(5);
-img_arr = ["../images/mountbierstadt.jpg", 
-           "../images/mountyale.jpg",
-           "../images/gardenofgods.jpg",
-           "../images/lakedillon.jpg",
-           "../images/mountelbert.jpg"
-           ];
+let background_img_paths = new Array(5);
+background_img_paths = ["../images/mountbierstadt.jpg", 
+                        "../images/mountyale.jpg",
+                        "../images/gardenofgods.jpg",
+                        "../images/lakedillon.jpg",
+                        "../images/mountelbert.jpg"
+                       ];
+profile_img_paths = ["images/noogler.gif",
+                     "images/zoebakercircle.png"
+                    ];
 labels = ["the summit of Mount Bierdstadt.", 
           "the summit of Mount Yale.", 
           "the Garden of the Gods.", 
           "Lake Dillon, seen from the vantage point of driving along Dillon Dam road.",
           "the summit of Mount Elbert, the tallest point in Colorado."];
 
+function preload_imgs(img_arr, img_paths){
+  for (let i = 0; i < img_paths.length; i++) {
+      img_arr[i] = new Image();
+      img_arr[i].src = img_paths[i];
+  }
+}
 
 $(document).ready(function(){
+    // preload all images to speedup image transitions
+  let all_image_paths = background_img_paths.concat(profile_img_paths)
+  preload_imgs(new Array(all_image_paths.length), all_image_paths)
+
     // if change background button is clicked, background image for the body is changed. 
     // Appropriate image label is displayed.
   let current_image_index = 0;
   $("#change-background").click(function(){
-      current_image_index = current_image_index % img_arr.length;
-      $("body").css("background-image", "url(" + img_arr[current_image_index] + ")");
+      current_image_index = current_image_index % background_img_paths.length;
+      $("body").css("background-image", "url(" + background_img_paths[current_image_index] + ")");
       $("#background-caption").html("This image is of "+ labels[current_image_index]);
       current_image_index++;            
   })
