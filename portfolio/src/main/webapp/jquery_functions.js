6baab77dee1f28d7358b19c8d3c5ecaba746eb1a
lzoebaker <lzoebaker@gmail.com>
2020-05-28 10:53:52 -0600

    Everything related to changing backgrounds is in a seperate .js file

--- portfolio/src/main/webapp/jquery_functions.js
+++ portfolio/src/main/webapp/jquery_functions.js
@@ -1,57 +1,32 @@
 /*
 * Purpose: contains various JQuery functions that listen for events on main portfolio page
 */
-// For each individual background img, width is 2016px height is 1512px
-const BACKGROUND_IMG_WIDTH = 2016;
-const BACKGROUND_IMG_HEIGHT = 1512;
-const VERT_PADDING = 40;
-const HORZ_PADDING = 20;
-const SPRITE_IMG_ROWS = 2;
-const SPRITE_IMG_COLS = 3;
-const NUM_IMAGES = 5;
 
-labels = ["the summit of Mount Bierdstadt.", 
-          "the summit of Mount Yale.", 
-          "the summit of Mount Elbert, the tallest point in Colorado.",
-          "the Garden of the Gods.", 
-          "Lake Dillon, seen from the vantage point of driving along Dillon Dam road."];
+import { update_background, NUM_IMAGES } from './background_functions.js';
 
-profile_img_paths = ["images/noogler.gif",
-                     "images/zoebakercircle.png"
-                    ];
+let profile_image_paths = ["images/noogler.gif", "images/zoebakercircle.png"];
 
-function preload_imgs(img_arr, img_paths){
-  for (let i = 0; i < img_paths.length; i++) {
-      img_arr[i] = new Image();
-      img_arr[i].src = img_paths[i];
+
+// Loads image paths into an arbitrary array, in order to preload images for improved performance.
+function preload_images(image_paths){
+  let image_arr = new Array(image_paths.length);
+  for (let i = 0; i < image_paths.length; i++) {
+    let temp_image = new Image(); 
+    temp_image.src = image_paths[i]; 
+    image_arr[i] = temp_image;
   }
+  return image_arr;
 }
 
+let current_image_index = 1; // start on second image so as not to repeat landing image
 $(document).ready(function(){
-    // preload profile icon images + gif
-    preload_imgs(new Array(2), profile_img_paths);
 
-    // if change background button is clicked, background image for the body is changed.
-    // each background image corresponds to a pixel location in a larger sprite image. 
-    // if change background button is clicked, background image for the body is changed. 
-  let current_image_index = 1; // start on second image so as not to repeat landing image
+  let icon_images = preload_images(profile_image_paths);
+
+  // if change background button is clicked, background image for the body is changed.
   $("#change-background").click(function(){
-      current_image_index = current_image_index % NUM_IMAGES;
-      
-      let row = Math.floor(current_image_index / SPRITE_IMG_COLS);
-      let vertical_offset = BACKGROUND_IMG_HEIGHT * row
-      vertical_offset += (row+1) * VERT_PADDING;
-      let col = current_image_index % SPRITE_IMG_COLS
-      let horizontal_offset = BACKGROUND_IMG_WIDTH * col
-      horizontal_offset += (col+1) * HORZ_PADDING;
-      
-      let string_background = "url('images/background_sprites.png') -" + horizontal_offset.toString()+ "px -" + vertical_offset.toString() +"px";
-      $("body").css("background", string_background);
-      $("body").css("background-attachment", "fixed");
-       
-      // Appropriate image label is displayed.
-      $("#background-caption").html("This image is of "+ labels[current_image_index]);
-      current_image_index++;            
+    update_background(current_image_index);
+    current_image_index = (current_image_index + 1) % NUM_IMAGES;
   });
 
   // if mouse enters profile image attribute, change image to animated noogler.gif. Once mouse leaves,
