7b272e8e11b4d0219ccf16ee0dc98e45e2d0f4e1
lzoebaker <lzoebaker@gmail.com>
2020-06-01 18:21:27 -0600

    Adding spaces between open braces

--- portfolio/src/main/webapp/comments.js
+++ portfolio/src/main/webapp/comments.js
@@ -2,20 +2,20 @@
 * Purpose: recieves HTTP promise response from CommentsServlet.java, places authors and comments into appropriate html container 
 */
 
-function getComment(){
+function getComment() {
     const responsePromise = fetch('/comments');
     // when server request complete, pass response into handleResponse
     responsePromise.then(handleResponse);
 }
 
-function handleResponse(response){
+function handleResponse(response) {
     // receives Java Object (ArrayList reponse)
     const commentListPromise = response.json();
     commentListPromise.then(addCommentsToDom);
 }
 
 /* Adds authors and comments to html page, displaying author: comment */
-function addCommentsToDom(commentList){
+function addCommentsToDom(commentList) {
     const commentContainer = document.getElementById('comments-container');
     commentContainer.innerHTML = '';
     for (let i = 0; i < commentList.length; i++){
