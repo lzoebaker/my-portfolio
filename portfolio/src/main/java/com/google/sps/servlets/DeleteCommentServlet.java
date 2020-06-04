01ac2589d55ca7a84d7f7238217e7810f85aadae
lzoebaker <lzoebaker@gmail.com>
2020-06-04 23:24:49 +0000

    Adding functionality to delete each comment

--- portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
@@ -5,6 +5,7 @@ import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.sps.data.Comment;
 import com.google.sps.data.CommentDatabase;
+import com.google.sps.servlets.ParameterGetter;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,15 +16,27 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/delete-comment")
 public class DeleteCommentServlet extends HttpServlet {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    private static final String KEY_VALUE_TO_DELETE = "key-value";
+    private static final String COMMENTS_PAGE_URL = "/comments-page.html";
+    private static final String AUTHOR_VALUE_DELINEATOR = ":";
+    
     @Override
       public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Comment comment = new Comment("hardcoded", "hardcoded"); // replace with getting user input from clicking a delete button
-        Key hardcodedKey = generateKey(comment);
-        datastore.delete(hardcodedKey);
+        String displayedComment = ParameterGetter.getParameter(request, KEY_VALUE_TO_DELETE);
+        Key commentKey = extractKeyFromDisplayedComment(displayedComment);
+        datastore.delete(commentKey);
+        response.sendRedirect(COMMENTS_PAGE_URL);
       }
-      
-      private Key generateKey(Comment comment) {
-        String keyString = comment.getAuthor() + comment.getValue();
+
+      private Key extractKeyFromDisplayedComment(String displayedComment) {
+        String[] authorAndValue = displayedComment.split(AUTHOR_VALUE_DELINEATOR, 2);
+        String author = authorAndValue[0].trim();  
+        String value = authorAndValue[1].trim();
+        return generateKeyFromAuthorValue(author, value);
+      }
+
+      private Key generateKeyFromAuthorValue(String author, String value) {
+        String keyString = author + value;
         return KeyFactory.createKey(CommentDatabase.COMMENT_QUERY_STRING, keyString);
       }
 }
--- portfolio/src/main/webapp/comments.js
+++ portfolio/src/main/webapp/comments.js
@@ -14,19 +14,57 @@ function handleResponse(response) {
     commentListPromise.then(addCommentsToDom);
 }
 
-/* Adds authors and comments to html page, displaying author: comment */
+/* Adds authors and comments to html page, displaying author: comment.*/
 function addCommentsToDom(commentList) {
     const commentContainer = document.getElementById('comments-container');
     commentContainer.innerHTML = '';
     for (let i = 0; i < commentList.length; i++){
         commentContainer.appendChild(
-            createComment(commentList[i].author, commentList[i].value));
+            createCommentAndDeleteButton(commentList[i].author, commentList[i].value));
     }
 }
 
-/** Creates an <li> element containing author: comment. */
+/** creates a comment element with inline delete button */
+function createCommentAndDeleteButton(author, value) {
+  const commentWrapper = document.createElement('div');
+  commentWrapper.classList.add('comment_div');
+  const commentElement = createComment(author, value);
+  commentWrapper.appendChild(commentElement);
+  commentWrapper.appendChild(createDeleteButton(commentElement));
+  $('.comment_div li, .comment_div button').css('display', 'inline-block');
+  return commentWrapper;
+}
+
+/** Creates an <li> element containing author: comment.*/
 function createComment(author, value) {
   const liElement = document.createElement('li');
   liElement.innerText = author + ": " + value;
   return liElement;
+}
+
+/** Creates a delete button linked to the deletion of each comment */
+function createDeleteButton(commentElement) {
+  const deleteButton = document.createElement('form');
+  deleteButton.action = '/delete-comment';
+  deleteButton.method = 'GET';
+  deleteButton.appendChild(createSubmitButton());
+  deleteButton.appendChild(createKeyValueField(commentElement));
+  return deleteButton;
+}
+
+/** creates the submit button that triggers sending the "author:value" text of comment*/
+function createSubmitButton() {
+  const submitButton = document.createElement('input');
+  submitButton.type = 'submit';
+  submitButton.value = 'delete';
+  return submitButton;
+}
+
+/** sets the value to be sent as "author:value", which is used to identify comment for deletion */
+function createKeyValueField(commentElement) {
+  const keyValueOfComment = document.createElement('input');
+  keyValueOfComment.type = 'hidden';
+  keyValueOfComment.value = commentElement.innerText;
+  keyValueOfComment.name = 'key-value';
+  return keyValueOfComment;
 }
